f164c05bb1409c04f371458420e30feb411f7aba
Humam Hawara <himoo1999@gmail.com>
2021-01-28 21:58:12 -0500

    activitiy 1 done (sausage dog)

--- activities/wheres-sausage-dog/js/script.js
+++ activities/wheres-sausage-dog/js/script.js
@@ -1,18 +1,20 @@
 "use strict";
 
-/**
-Title of Project
-Author Name
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
+let animalImages = [];
+let animals = [];
 
-/**
-Description of preload
-*/
+let sausageDogImage = undefined;
+let sausageDog = undefined;
+
 function preload() {
-
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`)
+    animalImages.push(animalImage);
+  }
+  sausageDogImage = loadImage(`assets/images/sausage-dog.png`)
 }
 
 
@@ -20,7 +22,19 @@ function preload() {
 Description of setup
 */
 function setup() {
+createCanvas(windowWidth, windowHeight);
 
+for (let i = 0; i < NUM_ANIMALS; i++) {
+  let x = random(0,width);
+  let y = random(0, height);
+  let animalImage = random(animalImages);
+  let animal = new Animal(x, y, animalImage);
+  animals.push(animal);
+  }
+
+  let x = random(0, width);
+  let y = random(0, height);
+  sausageDog = new SausageDog(x, y, sausageDogImage);
 }
 
 
@@ -28,5 +42,15 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(255, 255, 0);
 
+  for (let i = 0; i < animals.length; i++) {
+    animals[i].update();
+  }
+
+  sausageDog.update();
+}
+
+function mousePressed () {
+  sausageDog.mousePressed();
 }
