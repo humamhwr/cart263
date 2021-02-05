ea27e285e8ecc3e0d32140b2a92925dbf825f8e2
Humam Hawara <himoo1999@gmail.com>
2021-02-04 19:54:58 -0500

    i like french fries

---
+++ experiments/responsiveVoice/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+
+/**
+Description of setup
+*/
+function setup() {
+createCanvas(500,500)
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+background(0);
+}
+
+function mousePressed() {
+  responsiveVoice.speak("i like french fries")
+}
