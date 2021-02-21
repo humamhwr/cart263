0dc94e5c4314006e88e3198a82dd33533356c5a5
Humam Hawara <himoo1999@gmail.com>
2021-02-21 00:53:30 -0500

    name remember exp

--- experiments/web-storage/js/script.js
+++ experiments/web-storage/js/script.js
@@ -1,52 +1,36 @@
-// Track clicks
-let clicks = 0;
-// A place to store the game data
-let gameData = {
-  highScore: 0 // Start the high score at 0 by default
-};
+"use strict";
+
+let userData = {
+  name: `stranger` // A default value if we don't know the user's name
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // Try to load the game data (remembering to parse it first)
-  let data = JSON.parse(localStorage.getItem(`game-data`));
-  // Check if there's anything there
-  if (data !== null) {
-    // There is data! So replace our default game data with the save data
-    gameData = data;
+  // Try to load the user data
+  let data = JSON.parse(localStorage.getItem(`example-user-data`));
+  // Check if there's data there...
+  if (data) {
+    // If there is, then use the name property in the data
+    userData.name = data.name;
+  }
+  else {
+    // If there isn't, ask the user their name and store it in the user data
+    // prompt() brings up a simple dialog that the user can enter text in
+    // The first argument is the prompt for the user, the second argument is
+    // a default value to provide
+    userData.name = prompt(`What's ya name?`, `Tony`);
+    // Save the user data
+    localStorage.setItem(`example-user-data`, JSON.stringify(userData));
   }
 }
 
 function draw() {
-  background(0);
+  background(255);
 
-  // Display score
-  push();
-  textSize(64);
-  textAlign(CENTER);
-  textStyle(BOLD);
-  fill(255, 255, 0);
-  text(clicks, width / 2, height / 2);
-  pop();
-
-  // Display high score
+  // Greet the user according to their name
   push();
   textSize(32);
-  textAlign(LEFT, TOP);
-  textStyle(BOLD);
-  fill(255, 255, 0);
-  text(`High score: ${gameData.highScore}`, 0, 0);
-  pop();
-}
-
-function mousePressed() {
-  // They clicked!
-  clicks++;
-  // Check if this beats the current high score...
-  if (clicks > gameData.highScore) {
-    // Set the new high score
-    gameData.highScore = clicks;
-    // Save the game data to remember for next time, remembering to stringify the data first
-    localStorage.setItem(`game-data`, JSON.stringify(gameData));
-  }
+  textAlign(CENTER);
+  text(`Hi there, ${userData.name}.`, width / 2, height / 2);
 }
