1dcb142d00674bd65e6a146bd5000e2121977104
humamhawara <hummam.houara@mail.concordia.ca>
2021-03-19 21:55:47 -0400

    started bubble popper

---
+++ activities/bubble-popper/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+Bubble Popper
+Hummam Houara
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
