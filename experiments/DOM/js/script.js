193e32804c1569ac9962efcf987b08e2b911d814
humamhawara <hummam.houara@mail.concordia.ca>
2021-03-24 16:41:35 -0400

    DOM: mmouse click

--- experiments/DOM/js/script.js
+++ experiments/DOM/js/script.js
@@ -1,4 +1,11 @@
 let mainHeading = document.getElementById(`main-heading`);
-mainHeading.addEventListener(`click`, function(event) {
-  mainHeading.innerText = `${event.clientX},${event.clientY}`;
-});
+mainHeading.addEventListener(`click`, setRedTextColor);
+let subHeading = document.getElementById(`sub-heading`);
+subHeading.addEventListener(`click`, setRedTextColor);
+let paragraph = document.getElementById(`paragraph`);
+paragraph.addEventListener(`click`, setRedTextColor);
+
+function setRedTextColor(event) {
+  // Use event.target to change the style of the specific clicked element
+  event.target.style[`color`] = `#ff0000`;
+}
