8eaba17e3f9c60eb4ba2f845be8d6c12c937cc1d
humamhawara <hummam.houara@mail.concordia.ca>
2021-03-24 16:44:04 -0400

    contextmenu

--- experiments/DOM/js/script.js
+++ experiments/DOM/js/script.js
@@ -1,11 +1,5 @@
-let mainHeading = document.getElementById(`main-heading`);
-mainHeading.addEventListener(`click`, setRedTextColor);
-let subHeading = document.getElementById(`sub-heading`);
-subHeading.addEventListener(`click`, setRedTextColor);
 let paragraph = document.getElementById(`paragraph`);
-paragraph.addEventListener(`click`, setRedTextColor);
 
-function setRedTextColor(event) {
-  // Use event.target to change the style of the specific clicked element
+paragraph.addEventListener(`contextmenu`, function(event) {
   event.target.style[`color`] = `#ff0000`;
-}
+});
